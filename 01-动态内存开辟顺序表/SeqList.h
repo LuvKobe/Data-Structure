4f40e29cff6817513c5305f07ddba2c2ff0101f2
Edison <3078601436@qq.com>
2022-08-03 07:49:21 +0800

    顺序表

--- 01-动态内存开辟顺序表/SeqList.h
+++ 01-动态内存开辟顺序表/SeqList.h
@@ -1,73 +1,45 @@
-#pragma once 
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-// .h 放声明，用来看整个框架
 
-// assert断言 -->  条件为真，就没什么事；如果条件为假，就终止程序并报错，会精确的告诉你，那个文件的那一行出错了
-// return;  --> 结束函数
-// exit(-1) --> 结束程序
-
-
-// 要求存储的数据是从0开始，依次连续存储
-// 静态的顺序表
-// 问题：开小了，不够用；开大了，存在浪费
-
-//#define N 100
-//struct SeqList
-//{
-//	int a[N]; 
-//	int size; // 记录存储了多少个数据
-//};
-
-
-// SL：SeqList的缩写
-// DataType：数据类型
 typedef int SLDataType;
 
-// 动态的顺序表
 typedef struct SeqList
 {
-	SLDataType* a; // 指向动态开辟的数组
-	int size; // 存储的数据个数
-	int capacity; // 存储空间的大小
+	SLDataType* a;
+	int size; //元素个数
+	int capacity; //元素容量
 }SeqList;
 
+//初始化顺序表
+void SListInit(SeqList* ps);
 
-// 打印顺序表
-void SeqListPrint(SeqList* psl);
+//打印顺序表
+void SListPrint(SeqList* ps);
 
-// 顺序表初始化
-void SeqListInit(SeqList* psl);
+//尾插
+void SListPushBack(SeqList* ps, SLDataType x);
 
+//尾删
+void SListPopBack(SeqList* ps);
 
-// 尾插和尾删：时间复杂度O(1)
-// 顺序表尾插
-void SeqListPushBack(SeqList* psl, SLDataType x);
-// 顺序表尾删
-void SeqListPopBack(SeqList* psl);
+//头插
+void SListPushFront(SeqList* ps, SLDataType x);
 
+//头删
+void SListPopFront(SeqList* ps);
 
-// 头插和头删：时间复杂度O(N)
-// 顺序表头插
-void SeqListPushFront(SeqList* psl, SLDataType x);
-// 顺序表头删
-void SeqListPopFront(SeqList* psl);
+//顺序表查找
+int SListFind(SeqList* ps, SLDataType x);
 
-// 检查空间，如果满了，进行增容
-void SeqListCheckCapacity(SeqList* psl);
+//顺序表在pos位置插入x
+void SListInsert(SeqList* ps, int pos, SLDataType x);
 
-// 在pos位置插入x
-void SeqListInsert(SeqList* psl, size_t pos, SLDataType x);
-//删除pos位置的数据
-void SeqListErase(SeqList* psl, size_t pos);
+//顺序表删除在pos位置的值
+void SListErase(SeqList* ps, int pos);
 
-// 顺序表查找
-int SeqListFind(SeqList* psl, SLDataType x);
-// 顺序表修改
-//void SeqListModify(SeqList* psl, size_t pos, SLDataType x);
+void SListModify(SeqList* ps, int pos, SLDataType x);
 
-// 顺序表销毁
-void SeqListDestroy(SeqList* psl);
-
-
+//顺序表销毁
+void SListDestroy(SeqList* ps);
